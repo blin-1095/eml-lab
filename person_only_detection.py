7c567722364f98e4b07523be6d03213b2cc729c9
Daniel Zelesnov <daniel.zelesnov@gmail.com>
2026-07-19 02:11:37 +0200

    Finished first pruning implementation and added onnx model generation for pruned and person-only model

--- person_only_detection.py
+++ person_only_detection.py
@@ -5,19 +5,19 @@ from utils.dataloader import VOCDataLoader, VOCDataLoaderPerson
 from tinyyolov2 import TinyYoloV2
 from utils.loss import YoloLoss
 
-loader = VOCDataLoaderPerson(train=True, batch_size=1)
+loader = VOCDataLoaderPerson(train=True, batch_size=128)
 loader_test = VOCDataLoaderPerson(train=False, batch_size=1)
 
-device = 'cuda'
+DEVICE = 'cuda'
 
 # make instance with 20 classes as output
 net = TinyYoloV2(num_classes=1)
 
 # load pretrained weights
-sd = torch.load("4-challenge/voc_pretrained.pt")
+sd = torch.load("state_dicts/voc_pretrained.pt")
 net.load_state_dict({k: v for k, v in sd.items() if not '9' in k}, strict=False)
 
-net = net.to(device)
+net = net.to(DEVICE)
 net.eval()
 
 criterion = YoloLoss(anchors=net.anchors)
@@ -31,7 +31,7 @@ for key, param in net.named_parameters():
         param.requires_grad = False
 
 
-optimizer = torch.optim.Adam(filter(lambda x: x.requires_grad, net.parameters()), lr=0.001)
+optimizer = torch.optim.Adam(filter(lambda x: x.requires_grad, net.parameters()), lr=0.001) #type: ignore
 
 
 from utils.ap import precision_recall_levels, ap, display_roc
@@ -49,7 +49,7 @@ for epoch_n in range(NUM_EPOCHS):
 
         optimizer.zero_grad()
 
-        inputs, targets = inputs.to(device), targets.to(device)
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
 
         outputs = net(inputs, yolo=False)
         loss,_ = criterion(outputs, targets)
@@ -62,7 +62,7 @@ for epoch_n in range(NUM_EPOCHS):
     with torch.no_grad():
         for idx, (inputs, targets) in tqdm.tqdm(enumerate(loader_test), total=NUM_TEST_SAMPLES):
             
-            inputs, targets = inputs.to(device), targets.to(device)
+            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
             outputs = net(inputs, yolo=True)
             
             #The right threshold values can be adjusted for the target application
@@ -75,7 +75,7 @@ for epoch_n in range(NUM_EPOCHS):
             if idx == NUM_TEST_SAMPLES:
                 break
             
-            display_result(inputs.cpu(), outputs, targets.cpu(), file_path=f'yolo_prediction_{idx}.png')
+            # display_result(inputs.cpu(), outputs, targets.cpu(), file_path=f'yolo_prediction_{idx}.png')
                 
     #Calculation of average precision with collected samples
     test_AP.append(ap(test_precision, test_recall))
@@ -84,4 +84,23 @@ for epoch_n in range(NUM_EPOCHS):
     #plot ROC
     display_roc(test_precision, test_recall)
 
-torch.save(net.state_dict(), 'yolov2_person_finetuned.pt')
+torch.save(net.state_dict(), 'state_dicts/yolov2_person_finetuned.pt')
+
+# Export as onnx model
+net.eval()
+
+export_input, _ = next(iter(loader_test))
+export_input = export_input.to(DEVICE)
+
+torch.onnx.export(
+        net,
+        export_input,
+        "models/person_only_yolo.onnx",
+        export_params=True,
+        opset_version=11,
+        input_names=['input_image'],
+        output_names=['yolo_output'],
+
+        # dynamic axes so the model can be used with different batch sizes
+        dynamic_axes={'input_image': {0: 'batch_size'}, 'yolo_output': {0: 'batch_size'}}
+)
