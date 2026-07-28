b79ddeffe533562e32824f22cf372997344dc22b
Daniel Zelesnov <daniel.zelesnov@gmail.com>
2026-07-28 03:23:53 +0200

    Updated onnx util

--- utils/onnx_quantization.py
+++ utils/onnx_quantization.py
@@ -1,4 +1,6 @@
 import os
+import onnx
+from onnx.shape_inference import infer_shapes
 import onnxruntime as ort
 from onnxruntime.quantization import quantize_static, QuantType, CalibrationDataReader
 
@@ -15,7 +17,7 @@ class ONNXCalibrationReader(CalibrationDataReader):
         self.iterator = iter(dataloader)
         
         # Extract the exact input node name from the ONNX graph
-        session = ort.InferenceSession(onnx_model_path, providers=['CUDAExecutionProvider'])
+        session = ort.InferenceSession(onnx_model_path, providers=['TensorrtExecutionProvider'])
         self.input_name = session.get_inputs()[0].name
 
     def get_next(self) -> dict:
@@ -33,17 +35,27 @@ def apply_static_quantization(input_onnx_path: str, output_onnx_path: str, calib
     """
     os.makedirs(os.path.dirname(output_onnx_path), exist_ok=True)
     
+    # Force ONNX to calculate and save all intermediate shapes for TensorRT
+    model = onnx.load(input_onnx_path)
+    inferred_model = infer_shapes(model)
+    onnx.save(inferred_model, input_onnx_path)
+    
     # Initialize the data reader imported from dataloader.py
     data_reader = ONNXCalibrationReader(calib_loader, input_onnx_path)
     
-    # Perform Static Quantization
+    # Perform Static Quantization with strict TensorRT requirements via extra_options
     quantize_static(
         model_input=input_onnx_path,
         model_output=output_onnx_path,
         calibration_data_reader=data_reader,
         quant_format=ort.quantization.QuantFormat.QDQ,  #type: ignore
         weight_type=QuantType.QInt8,
-        activation_type=QuantType.QUInt8
+        activation_type=QuantType.QInt8,
+        extra_options={
+            'ActivationSymmetric': True,
+            'WeightSymmetric': True,
+            'QuantizeBias': False # prevents int32 fuckery, we fused the bias with batchnorm already anyway
+        }
     )
     
     orig_size = os.path.getsize(input_onnx_path) / (1024 * 1024)
